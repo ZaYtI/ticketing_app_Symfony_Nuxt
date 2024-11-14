892a948ded2dbb1f1bf2f431e502e5ba9c597fa4
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-11-14 10:03:25 +0100

    🎨 Update methods index for ticket

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -101,10 +101,14 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
+    }
+
+    public function isAdmin(): bool
+    {
+        return in_array('ROLE_ADMIN', $this->getRoles());
     }
 
     /**
