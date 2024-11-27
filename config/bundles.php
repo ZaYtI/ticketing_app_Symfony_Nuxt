eea7d1e7ff7a3259db2c64d0218c4c8e744a760a
ZaYtI <antonin_riquart@ens.univ-artois.fr>
2024-11-27 12:24:00 +0100

    ✨ Allow cors origin

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
